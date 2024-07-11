1d3d63a67118aa49d5190ac518bf23d31048ec25
jiwoo <1114jiwoo@gmail.com>
2024-07-11 20:25:33 +0900

    🩹 fix : 아이콘 추가

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -7,9 +7,9 @@ function Board() {
       <h1>보드 이름</h1>
       <p>보드 한 줄 설명</p>
       <div className="columns">
-        <Column title="To Do" cards={[{text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.", user: "OOO 님"}, {text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.", user: "OOO 님"}]} />
-        <Column title="In Progress" cards={[{text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.", user: "OOO 님"}, {text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.", user: "OOO 님"}]} />
-        <Column title="Done" cards={[{text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.", user: "OOO 님"}, {text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.", user: "OOO 님"}]} />
+        <Column title="🗒️ To Do" cards={[{text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.", user: "OOO 님"}, {text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.", user: "OOO 님"}]} />
+        <Column title="💻 In Progress" cards={[{text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.", user: "OOO 님"}, {text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.", user: "OOO 님"}]} />
+        <Column title="🚀 Done" cards={[{text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.", user: "OOO 님"}, {text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.", user: "OOO 님"}]} />
       </div>
     </div>
   );
