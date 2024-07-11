829acd4d968b88c19df292a913eed128765e55f4
jiwoo <1114jiwoo@gmail.com>
2024-07-11 17:37:14 +0900

    ✨ feat : 폴더 구조 변경

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/main" element={<Main />} />
+        </Routes>
+      </Router>
     </div>
+  );
+}
+
+function Home() {
+  return (
+    <header className="App-header">
+      <h1>Main Page</h1>
+      <nav>
+        <a href="/main">Go to Main Page</a>
+      </nav>
+    </header>
   );
 }
 
