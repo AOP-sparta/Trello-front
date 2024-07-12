52df33c0e435e30a1bf5efb53d8107e0bf23aada
jiwoo <1114jiwoo@gmail.com>
2024-07-12 10:40:00 +0900

    [#6] ✨ feat : 로그인 회원가입 버튼 클릭시 해당 페이지로 이동

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from '../styles/Header.module.css';
 
 function Header() {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate('/login');
+  };
+
+  const handleSignupClick = () => {
+    navigate('/signup');
+  };
+
   return (
     <header className={styles.header}>
-      <div className={styles.headerTitle}>Trello</div>
+      <Link to="/board" className={styles.headerTitle}>Trello</Link>
       <div className={styles.headerButtons}>
-        <button>로그인</button>
-        <button>회원가입</button>
+        <button onClick={handleLoginClick}>로그인</button>
+        <button onClick={handleSignupClick}>회원가입</button>
       </div>
     </header>
   );
