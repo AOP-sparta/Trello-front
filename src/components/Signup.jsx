69eda3fc0323e28836478a96a274ab3726092ec0
heKim <khu4237@gmail.com>
2024-07-12 20:40:50 +0900

    [#18] ✨ feat : Front-Back 연동 (React)
    cors 설정
    테스트 완료
    프록시 적용 되지 않으나 일단 풀api 주소 적었을 때 작동 되므로 커밋하고 시간 남으면 수정하겠습니다.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,24 +1,40 @@
 import React from 'react';
+import { useRef, } from 'react' 
 import styles from '../styles/Signup.module.css';
 import { useNavigate } from 'react-router-dom';
+import axios from "axios"
 
 function Signup() {
+    const emailInput = useRef();
+    const passwordInput = useRef();
+    const nicknameInput = useRef();
+
     const navigate = useNavigate();
 
-    const handleBoardClick = () => {
-        navigate('/board');
+    const handleSignupClick = () => {
+        axios.post(`http://localhost:8080/users/signup`, {
+            email: emailInput.current.value,
+            password: passwordInput.current.value,
+            nickname: nicknameInput.current.value
+        })
+            .then(rsp => {
+                if (rsp.data.msg == "회원가입 성공") {
+                    console.log(rsp.data.result);
+                    navigate('/board');
+                }
+            });
     };
 
     return (
         <div className={styles.signup}>
             <h2 className={styles.title}>회원가입</h2>
-            <form className={styles.form}>
-                <input className={styles.input} type="email" placeholder="Email" />
-                <input className={styles.input} type="password" placeholder="Password" />
+            <div className={styles.form}>
+                <input className={styles.input} type="email" placeholder="Email" ref={emailInput} />
+                <input className={styles.input} type="password" placeholder="Password" ref={passwordInput} />
                 <input className={styles.input} type="password" placeholder="Repeat Password" />
-                <input className={styles.input} type="text" placeholder="User Name" />
-                <button className={styles.button} type="submit" onClick={handleBoardClick}>Sign up</button>
-            </form>
+                <input className={styles.input} type="text" placeholder="User Name" ref={nicknameInput} />
+                <button className={styles.button} type="submit" onClick={handleSignupClick}>Sign up</button>
+            </div>
         </div>
     );
 }
