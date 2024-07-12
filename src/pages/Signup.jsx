2b5af6ed0fa324af5701c67b344cdea452bb457b
heKim <khu4237@gmail.com>
2024-07-12 16:40:34 +0900

    [#1] ✨ feat : 회원가입 화면 구성
    회원가입 컴포넌트 html 작성
    회원가입 css 모듈 추가

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import styles from '../styles/Signup.module.css';
 
 function Signup() {
   return (
-    <div className="signup">
-      <h2>회원가입 페이지</h2>
+    <div className={styles.signup}>
+      <h2 className={styles.title}>회원가입</h2>
+      <form className={styles.form}>
+        <input className={styles.input} type="email" placeholder="Email" />
+        <input className={styles.input} type="password" placeholder="Password" />
+        <input className={styles.input} type="password" placeholder="Repeat Password" />
+        <input className={styles.input} type="text" placeholder="User Name" />
+        <button className={styles.button} type="submit">Sign up</button>
+      </form>
     </div>
   );
 }
