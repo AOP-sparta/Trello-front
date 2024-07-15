585e38d7f444994b3151fa35292924ca11b20d6a
heKim <khu4237@gmail.com>
2024-07-15 11:27:40 +0900

    [#21] ✨ feat : 회원가입 예외처리
    채워지지 않은 칸 있을 때 alert
    비밀번호, 비밀번호 확인이 일치하는지 검증 후 일치하지 않을 경우 signup 버튼 눌렀을 때 alert
    리퀘스트 응답으로 validation 에러 떴을 시 alert
    리퀘스트 응답으로 중복 email 에러 떴을 시 alert

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useRef, } from 'react' 
+import { useRef, useEffect } from 'react' 
 import styles from '../styles/Signup.module.css';
 import { useNavigate } from 'react-router-dom';
 import axios from "axios"
@@ -7,22 +7,40 @@ import axios from "axios"
 function Signup() {
     const emailInput = useRef();
     const passwordInput = useRef();
+    const repeatPasswordInput = useRef();
     const nicknameInput = useRef();
 
     const navigate = useNavigate();
 
+    useEffect(() => {
+        console.log(emailInput.current, passwordInput.current, repeatPasswordInput.current, nicknameInput.current);
+    }, []);
+
     const handleSignupClick = () => {
+        if (!emailInput.current || !passwordInput.current || !repeatPasswordInput.current || !nicknameInput.current) {
+            alert('빈 칸을 전부 입력 해주세요.');
+            return;
+        }
+
+        if(repeatPasswordInput.current.value !== passwordInput.current.value) {
+            alert('비밀번호 확인이 일치하지 않습니다.');
+            return;
+        }
+
         axios.post(`http://localhost:8080/users/signup`, {
             email: emailInput.current.value,
             password: passwordInput.current.value,
             nickname: nicknameInput.current.value
         })
-            .then(rsp => {
-                if (rsp.data.msg == "회원가입 성공") {
-                    console.log(rsp.data.result);
-                    navigate('/board');
-                }
-            });
+        .then(rsp => {
+            if (rsp.data.msg === "회원가입 성공") {
+                console.log(rsp.data.result);
+                navigate('/board');
+            } else {}
+        })
+        .catch(error => {
+            alert(`${error.response.data.msg}`)
+        });
     };
 
     return (
@@ -31,7 +49,7 @@ function Signup() {
             <div className={styles.form}>
                 <input className={styles.input} type="email" placeholder="Email" ref={emailInput} />
                 <input className={styles.input} type="password" placeholder="Password" ref={passwordInput} />
-                <input className={styles.input} type="password" placeholder="Repeat Password" />
+                <input className={styles.input} type="password" placeholder="Repeat Password" ref={repeatPasswordInput} />
                 <input className={styles.input} type="text" placeholder="User Name" ref={nicknameInput} />
                 <button className={styles.button} type="submit" onClick={handleSignupClick}>Sign up</button>
             </div>
